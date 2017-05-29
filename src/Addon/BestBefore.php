c80e20e4ab474749e23c5f9baa1f412f4dc471ab
Mark Parnell <markparnell@brownbox.net.au>
2017-05-30 09:45:37 +1000

    Whitespace cleanup

--- src/Addon/BestBefore.php
+++ src/Addon/BestBefore.php
@@ -7,17 +7,6 @@ use BrownBox\Express\Base as Base;
 use BrownBox\Express\Helper as Helper;
 
 class BestBefore extends Base\Addon implements Interfaces\Addon {
-
-	/**
-	 * Queries to create custom database tables
-	 *
-	 * @var array
-	 * @access protected
-	 */
-	protected $_custom_db_table_queries = array(
-
-	);
-
 	/**
 	 * List of post types covered by Best Before addon
 	 *
@@ -25,15 +14,14 @@ class BestBefore extends Base\Addon implements Interfaces\Addon {
 	 * @access private
 	 */
 	protected $_post_types_to_covered = array(
-		'post',
-		'page'
+    		'post',
+    		'page',
 	);
 
 	/**
      * Class constructor
      */
     public function __construct() {
-
         $this->_name = __( 'Best Before', 'bb' );
         $this->_description = __( 'Introduce expiry dates for posts', 'bb' );
         $this->_current_version = '1.0';
@@ -42,24 +30,17 @@ class BestBefore extends Base\Addon implements Interfaces\Addon {
 
         $this->set_dependencies($dependencies);
 
-		add_action( 'init', array($this, 'frontend_hooks'));
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts') );
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_frontend_scripts') );
-
+		add_action('init', array($this, 'frontend_hooks'));
+		add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_scripts'));
+		add_action('wp_enqueue_scripts', array($this, 'enqueue_frontend_scripts'));
     }
 
 	/**
 	 * Execute cron
 	 */
 	public function execute_cron() {
-
-		// bb_log( 'Started the "Best Before" cron');
-
 		$this->send_expiry_notification();
 		$this->unpublish_expired_posts();
-
-		// bb_log( 'Finished the "Best Before" cron');
-
 	}
 
 	/**
@@ -68,15 +49,11 @@ class BestBefore extends Base\Addon implements Interfaces\Addon {
 	 * @access public
 	 */
 	public function frontend_hooks() {
-
 		if ( ! is_admin() ) {
-
 			// add_action( 'pre_get_posts', [ $this, 'pre_get_posts_filter' ] );
 			// add_filter( 'posts_results', [ $this, 'posts_results_filter' ] );
 			// add_action( 'shutdown', [ $this, 'process_collected_data' ] );
-
 		}
-
 	}
 
 	/**
@@ -86,10 +63,8 @@ class BestBefore extends Base\Addon implements Interfaces\Addon {
 	 * @since 1.0
 	 */
 	public static function enqueue_admin_scripts() {
-
 		wp_enqueue_style( 'best-before-css', BB_EXPRESS_ADDONS_URL . '/BestBefore/css/best-before.css', array(), filemtime( BB_EXPRESS_ADDONS_URL . '/BestBefore/css/best-before.css' ) );
 		wp_enqueue_script( 'best-before-js', BB_EXPRESS_ADDONS_URL . '/BestBefore/js/best-before.js', array('jquery'), '0.0.1', true );
-
 	}
 
 	/**
@@ -106,7 +81,6 @@ class BestBefore extends Base\Addon implements Interfaces\Addon {
 	 * @access public
 	 */
 	public function send_expiry_notification() {
-
 		$expired_posts = $this->get_expired_posts();
 
 		$subject = 'Posts that need your attention';
@@ -116,7 +90,6 @@ class BestBefore extends Base\Addon implements Interfaces\Addon {
 		// Set post to draft
 		// bb_log( $content, 'Sending an email to ' . $recipient );
 		// Helper\Email::send_email( $recipient, $subject, $content );
-
 	}
 
 	/**
@@ -127,13 +100,11 @@ class BestBefore extends Base\Addon implements Interfaces\Addon {
 	 * @access private
 	 */
 	private function _generate_expiry_notification_content( $expired_posts ) {
-
 		$html = '';
 		$expired_posts_html = '<ul>';
 		$current_date = time();
 
 		foreach ( $expired_posts as $expired_post ) {
-
 			// Only automatically send an email if the auto un-publish option is disabled for this post
 			if ( ! $expired_post->bbx_best_before_unpublish_when_expired || empty( $expired_post->bbx_best_before_unpublish_when_expired ) ) {
 
@@ -146,25 +117,20 @@ class BestBefore extends Base\Addon implements Interfaces\Addon {
 				// Generate URL for editing a post
 				$edit_post_url = \get_bloginfo('url') . '/wp-admin/post.php?post=' . $expired_post->ID . '&action=edit';
 				$expired_posts_html .= '<li>"' . $expired_post->post_title . '" post expired ' . $converted_time['days'] . ' day(s) ago. <a href="' . $edit_post_url . '">Edit this post.</a></li>';
-
 			}
-
 		}
 
 		$expired_posts_html .= '</ul>';
 
 		$html .= <<<MULTI
-
 <h2>Hi there</h2>
 <p>Please see below a list of posts that have expired and require your attention.</p>
 {$expired_posts_html}
 <p>Please note: all the posts in the list are not enabled to auto-unpublish once expired.</p>
-<p>Kind regards,<br/>Your site</p>
-
+<p>Kind regards,<br>Your site</p>
 MULTI;
 
 		return $html;
-
 	}
 
 	/**
@@ -173,14 +139,10 @@ MULTI;
 	 * @access public
 	 */
 	public function unpublish_expired_posts() {
-
 		$expired_posts = $this->get_expired_posts();
-
 		foreach ( $expired_posts as $expired_post ) {
-
 			// Only automatically unpublish if the corresponding option is enabled for this post
 			if ( $expired_post->bbx_best_before_unpublish_when_expired ) {
-
 				// Set post to draft
 				wp_update_post(
 					array(
@@ -188,11 +150,8 @@ MULTI;
 						'post_status'   =>  'draft'
 					)
 				);
-
 			}
-
 		}
-
 	}
 
 	/**
@@ -201,11 +160,9 @@ MULTI;
 	 * @return array
 	 */
 	public function get_expired_posts() {
-
 		$today = date("Y-m-d");
 
-		$args = [
-
+		$args = array(
 			'post_type' => $this->_post_types_to_covered,
 			'meta_key' => 'bbx_best_before_expiry_date',
 			'orderby'   => 'meta_value',
@@ -219,13 +176,11 @@ MULTI;
 					'type' => 'DATE'
 				)
 			)
-
-		];
+		);
 
 		$expired_posts = get_posts( $args );
 
 		return $expired_posts;
-
 	}
 
 	/**
@@ -235,9 +190,7 @@ MULTI;
 	 * @access public
 	 */
 	public function set_post_types_covered( $value ) {
-
 		$this->_post_types_to_covered = $value;
-
 	}
 
 	/**
@@ -247,11 +200,9 @@ MULTI;
 	 * @access public
 	 */
 	public function add_to_post_types_covered( $post_type ) {
-
 		if ( ! in_array( $post_type, $this->_post_types_to_covered ) ) {
 			$this->_post_types_to_covered[] = $post_type;
 		}
-
 	}
 
 	/**
@@ -262,11 +213,6 @@ MULTI;
 	 * @return array
 	 */
 	public function get_post_types_covered() {
-
 		return $this->_post_types_to_covered;
-
 	}
-
 }
-
-
