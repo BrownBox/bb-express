548f86ee7fd69b002dbefbb2174fe76fc32e3851
Mark Parnell <markparnell@brownbox.net.au>
2017-07-05 14:20:15 +1000

    Force WP to set envelope ID correctly on outgoing emails

--- bb-express.php
+++ bb-express.php
@@ -89,3 +89,8 @@ add_action('init', 'bb_express_run', 1);
 
 // $bb_express->get_all_addons();
 // $bb_express->launch();
+
+add_action('phpmailer_init', 'bb_set_mail_envelope_id');
+function bb_set_mail_envelope_id($phpmailer) {
+    $phpmailer->Sender = $phpmailer->From;
+}
